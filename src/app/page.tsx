82af3ff8eeeb11696b0ebef5e021230fc6598820
Adriel Garcia <adrielgarcia@live.com>
2025-03-26 19:42:13 -0300

    feat: create select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { Mail } from 'lucide-react'
 import { Input } from '@/components/Input'
 import { SettingsTabs } from '@/components/SettingsTabs'
 import * as FileInput from '@/components/Form/FileInput'
+import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -117,7 +119,11 @@ export default function Home() {
               Country
             </label>
 
-            <div />
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
@@ -128,7 +134,14 @@ export default function Home() {
               Timezone
             </label>
 
-            <div />
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+
+              <SelectItem value="utc3" text="América São Paulo (UTC-03:00)" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
