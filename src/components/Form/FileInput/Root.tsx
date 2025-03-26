1f28c956888506cad2a948a56c55bd6f5d12cb0c
Adriel Garcia <adrielgarcia@live.com>
2025-03-26 19:05:01 -0300

    feat: create multiple files upload

--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -13,7 +13,7 @@ interface RootProps extends ComponentProps<'div'> {}
 interface FileInputContextType {
   id: string
   files: File[]
-  onFilesSelected(files: File[]): void
+  onFilesSelected(files: File[], multiple: boolean): void
 }
 
 const FileInputContext = createContext({} as FileInputContextType)
@@ -23,7 +23,13 @@ export function Root(props: RootProps) {
 
   const [files, setFiles] = useState<File[]>([])
 
-  function onFilesSelected(files: File[]) {
+  function onFilesSelected(files: File[], multiple: boolean) {
+    if (multiple) {
+      setFiles((prevFiles) => [...prevFiles, ...files])
+
+      return
+    }
+
     setFiles(files)
   }
 
--- src/components/Form/FileInput/index.ts
+++ src/components/Form/FileInput/index.ts
@@ -2,5 +2,6 @@ import { Root } from './Root'
 import { Control } from './Control'
 import { ImagePreview } from './ImagePreview'
 import { Trigger } from './Trigger'
+import { FileList } from './FileList'
 
-export { Root, Control, Trigger, ImagePreview }
+export { Root, Control, Trigger, ImagePreview, FileList }
