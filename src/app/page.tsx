49acfa102b8c5f52b05b99cc7fd2240cdd26d68f
Adriel Garcia <adrielgarcia@live.com>
2025-03-27 20:30:27 -0300

    feat: create components variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import * as FileInput from '@/components/Form/FileInput'
 import { Select } from '@/components/Form/Select'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { Textarea } from '@/components/Form/Textarea'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -24,17 +25,11 @@ export default function Home() {
           </div>
 
           <div className="flex items-center gap-2">
-            <button className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 hover:bg-zinc-50">
-              Cancel
-            </button>
+            <Button variant="outline">Cancel</Button>
 
-            <button
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white hover:bg-violet-700"
-              type="submit"
-              form="settings"
-            >
+            <Button variant="primary" type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -165,43 +160,28 @@ export default function Home() {
                 </Select>
 
                 <div className="flex items-center gap-1">
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  <Button variant="ghost" type="button">
                     <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
+                  </Button>
 
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  <Button variant="ghost" type="button">
                     <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
+                  </Button>
 
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  <Button variant="ghost" type="button">
                     <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
+                  </Button>
 
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  <Button variant="ghost" type="button">
                     <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
+                  </Button>
 
-                  <button
-                    className="rounded-md p-2 hover:bg-zinc-50"
-                    type="button"
-                  >
+                  <Button variant="ghost" type="button">
                     <ListOrdered
                       className="h-4 w-4 text-zinc-500"
                       strokeWidth={3}
                     />
-                  </button>
+                  </Button>
                 </div>
               </div>
 
@@ -233,16 +213,11 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 hover:bg-zinc-50">
-              Cancel
-            </button>
+            <Button>Cancel</Button>
 
-            <button
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white hover:bg-violet-700"
-              type="submit"
-            >
+            <Button variant="primary" type="submit">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,8 +1,9 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <img
         src="https://github.com/adrielgarcias.png"
         className="h-10 w-10 rounded-full"
@@ -19,9 +20,9 @@ export function Profile() {
         </span>
       </div>
 
-      <button className="ml-auto rounded-md p-2 hover:bg-zinc-50" type="button">
+      <Button variant="ghost" type="button">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
